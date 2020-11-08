b91028892b1bde6ca917cb9ccc3948f038a5932e
PtrHtn <ptrhtn@gmail.com>
2020-11-07 22:36:23 -0300

    Add landscape_layout.dart

---
+++ lib/pages/home/widgets/landscape_layout.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LandscapeLayout extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      body: LandscapeLayoutBody(),
+    );
+  }
+}
+
+class LandscapeLayoutBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+} 
