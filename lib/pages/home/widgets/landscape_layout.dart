2f5e2e4a3dee10fe745b461451f3b4b64294e30b
PtrHtn <ptrhtn@gmail.com>
2020-11-09 15:13:58 -0300

    Update landscape_layout.dart

--- lib/pages/home/widgets/landscape_layout.dart
+++ lib/pages/home/widgets/landscape_layout.dart
@@ -1,9 +1,9 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../home_widgets.dart';
+
 class LandscapeLayout extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +16,26 @@ class LandscapeLayout extends StatelessWidget {
 class LandscapeLayoutBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Container(
+      color: Color(0xFF737373),
+      child: Container(
+        margin: EdgeInsets.all(40),
+        decoration: BoxDecoration(
+          color: Theme.of(context).cardColor,
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 25, left: 25),
+            ),
+            ImagePickerButton(
+              onPressed: null,
+            ),
+          ],
+        ),
+      ),
+    );
   }
-} 
+}
