c4313ca9a8868a3714a53c83137c1ee89dab203a
PtrHtn <ptrhtn@gmail.com>
2020-11-09 15:14:06 -0300

    Update portrait_layout.dart

--- lib/pages/home/widgets/portrait_layout.dart
+++ lib/pages/home/widgets/portrait_layout.dart
@@ -2,6 +2,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../home_widgets.dart';
+
 class PortraitLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -28,7 +30,28 @@ class PortraitLayout extends StatelessWidget {
 class PortraitLayoutBody extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+
+    return Container(
+      color: Color(0xFF737373),
+      child: Container(
+        margin: EdgeInsets.all(40),
+        decoration: BoxDecoration(
+          color: Theme
+              .of(context)
+              .cardColor,
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 25, left: 25),
+            ),
+            ImagePickerButton(onPressed: null,),
+          ],
+        ),
+      ),
+    );
+
   }
 }
