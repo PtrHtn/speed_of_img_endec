1feb5ae18e646101523ae692a6015257071fb2e1
PtrHtn <ptrhtn@gmail.com>
2020-11-11 19:00:47 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,27 @@
-void main() {}
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+
+import 'pages/home/home_binding.dart';
+import 'pages/home/home_page.dart';
+
+void main() {
+  runApp(SpeedOfImageEcDecApp());
+}
+
+class SpeedOfImageEcDecApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+
+      debugShowCheckedModeBanner: false,
+      title: 'Image Averaging',
+      theme: ThemeData.dark(),
+      initialRoute: '/home_page',
+      getPages: [
+        GetPage(name: '/home_page', page: () => HomePage(), binding: HomeBinding()),
+      ],
+    );
+  }
+}
