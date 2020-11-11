97042a15d446bd71d81c6814a022c06356721134
PtrHtn <ptrhtn@gmail.com>
2020-11-11 19:00:37 -0300

    Update image_picker_button.dart

--- lib/pages/home/widgets/image_picker_button.dart
+++ lib/pages/home/widgets/image_picker_button.dart
@@ -10,18 +10,20 @@ class ImagePickerButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: FloatingActionButton(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(
-            Radius.circular(16.0),
+      child: Center(
+        child: FloatingActionButton(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(
+              Radius.circular(16.0),
+            ),
           ),
+          backgroundColor: Get.theme.buttonColor,
+          child: Icon(
+            Icons.add_photo_alternate,
+            size: 48,
+          ),
+          onPressed: onPressed,
         ),
-        backgroundColor: Get.theme.buttonColor,
-        child: Icon(
-          Icons.add_photo_alternate,
-          size: 48,
-        ),
-        onPressed: onPressed,
       ),
     );
   }
