486995cb41271f38f0624a2183f2f2588746899c
PtrHtn <ptrhtn@gmail.com>
2020-11-12 22:13:57 -0300

    Add onPressedImagePickerButton method

--- lib/pages/home/home_controller.dart
+++ lib/pages/home/home_controller.dart
@@ -1,3 +1,23 @@
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 
-class HomeController extends GetxController{}
+class HomeController extends GetxController{
+
+  String imageAddress;
+
+
+  Future onPressedImagePickerButton(BuildContext context) async {
+    FilePickerResult result = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+      allowMultiple: false,
+      allowedExtensions: ['jpg', 'png'],
+    );
+
+    if (result != null) {
+      imageAddress = result.paths.map((path) => path).toString();
+
+    }
+  }
+
+}
