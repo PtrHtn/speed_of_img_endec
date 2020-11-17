c2f9b7b69c9b70a93c5405837d6be398ef413696
PtrHtn <ptrhtn@gmail.com>
2020-11-16 21:31:58 -0300

    Add portrait_layout.dart

---
+++ lib/pages/results/widgets/portrait_layout.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:speed_of_img_endec/pages/home/home_controller.dart';
+
+import '../results_controller.dart';
+
+
+class PortraitLayout extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              '  Speed of image En/Dec',
+            ),
+          ],
+        ),
+      ),
+      body: PortraitLayoutBody(),
+    );
+
+  }
+}
+
+class PortraitLayoutBody extends GetView<ResultsController>{
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      color: Color(0xFF737373),
+      child: Container(
+        margin: EdgeInsets.all(40),
+        decoration: BoxDecoration(
+          color: Theme
+              .of(context)
+              .cardColor,
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Padding(padding: const EdgeInsets.only(right: 25, left: 25)),
+          ],
+        ),
+      ),
+    );
+
+  }
+}
