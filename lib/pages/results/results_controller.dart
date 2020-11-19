c5a022eb8493370d7b0853f755b43f6526e773e3
PtrHtn <ptrhtn@gmail.com>
2020-11-18 21:10:20 -0300

    Add onInit method to the results controller to get arguments from home context

--- lib/pages/results/results_controller.dart
+++ lib/pages/results/results_controller.dart
@@ -2,4 +2,13 @@ import 'package:get/get.dart';
 
 class ResultsController extends GetxController{
 
+  String imageAddress;
+
+  @override
+  void onInit() {
+    super.onInit();
+
+    imageAddress = Get.arguments[0];
+  }
+
 }
