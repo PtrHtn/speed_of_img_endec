81f1c29bdb252d85df917de4d94a31677c407f73
PtrHtn <ptrhtn@gmail.com>
2020-11-19 21:52:35 -0300

    Update results_page.dart

--- lib/pages/results/results_page.dart
+++ lib/pages/results/results_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:speed_of_img_endec/pages/results/results_widgets.dart';
 
 class ResultsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
     return OrientationBuilder(builder: (context, orientation) {
-      return null;
+      return orientation == Orientation.landscape
+          ? LandscapeLayout()
+          : PortraitLayout();
     });
   }
 
