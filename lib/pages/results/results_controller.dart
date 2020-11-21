316beb5635979a3bbdd711039474af6159dcf24e
PtrHtn <ptrhtn@gmail.com>
2020-11-21 20:18:05 -0300

    Add imageResizing method to results_controller.dart

--- lib/pages/results/results_controller.dart
+++ lib/pages/results/results_controller.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:get/get.dart';
+import 'package:image/image.dart';
 
 class ResultsController extends GetxController{
 
@@ -9,6 +12,16 @@ class ResultsController extends GetxController{
     super.onInit();
 
     imageAddress = Get.arguments[0];
+  }
+
+  void imageResizing(){
+
+    final image = decodeImage(File(imageAddress).readAsBytesSync());
+    final imageHeight = image.height;
+    final newImageHeight = ((imageHeight / 100) * 80).round();
+    final resizedImage = copyResize(image, height: newImageHeight);
+
+    File('resizedImage.png').writeAsBytesSync(encodePng(resizedImage));
   }
 
 }
