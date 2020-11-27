e289d093180f104f105a419c7eedb81f3ea03b8c
PtrHtn <ptrhtn@gmail.com>
2020-11-27 18:55:12 -0300

    Remove Padding portrait_layout.dart

--- lib/pages/results/widgets/portrait_layout.dart
+++ lib/pages/results/widgets/portrait_layout.dart
@@ -4,11 +4,9 @@ import 'package:speed_of_img_endec/pages/home/home_controller.dart';
 
 import '../results_controller.dart';
 
-
 class PortraitLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       resizeToAvoidBottomPadding: false,
       appBar: AppBar(
@@ -24,38 +22,33 @@ class PortraitLayout extends StatelessWidget {
       ),
       body: PortraitLayoutBody(),
     );
-
   }
 }
 
-class PortraitLayoutBody extends GetView<ResultsController>{
+class PortraitLayoutBody extends GetView<ResultsController> {
   @override
   Widget build(BuildContext context) {
-
     return Container(
       color: Color(0xFF737373),
       child: Container(
         margin: EdgeInsets.all(40),
         decoration: BoxDecoration(
-          color: Theme
-              .of(context)
-              .cardColor,
+          color: Theme.of(context).cardColor,
           borderRadius: BorderRadius.all(Radius.circular(20)),
         ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Padding(padding: const EdgeInsets.only(right: 25, left: 25)),
-        Obx(() => Center(
-          child: controller.imageProcessing.value
-          ? Text("Processing")
-          : Text("Done"),
-        ))
+            Obx(() => Center(
+                  child: controller.imageProcessing.value
+                      ? Text("Processing")
+                      : Text("Done"),
+                ),
+            ),
           ],
         ),
       ),
     );
-
   }
 }
