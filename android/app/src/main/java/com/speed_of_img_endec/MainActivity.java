eea66f89b826ebb7f01077d164386d036b4ee97d
PtrHtn <ptrhtn@gmail.com>
2020-12-01 19:10:31 -0300

    Add configureFlutterEngine method to MainActivity.java

--- android/app/src/main/java/com/speed_of_img_endec/MainActivity.java
+++ android/app/src/main/java/com/speed_of_img_endec/MainActivity.java
@@ -1,6 +1,47 @@
 package com.speed_of_img_endec;
 
+import android.Manifest;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.BatteryManager;
+import android.os.Build;
+import android.os.Environment;
+import android.os.SystemClock;
+
+import androidx.annotation.NonNull;
+
+import java.io.File;
+
 import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugins.GeneratedPluginRegistrant;
 
 public class MainActivity extends FlutterActivity {
+
+    private static final String CHANNEL = "speed_of_img_endec";
+
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL).setMethodCallHandler(
+                (call, result) -> {
+
+                    switch (call.method) {
+
+                        case "imageDecoding":
+
+                            break;
+
+                        default:
+                            result.notImplemented();
+                            break;
+                    }
+                }
+        );
+    }
+
 }
