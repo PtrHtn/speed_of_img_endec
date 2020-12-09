8cb6d4d8edbafbeeb53979ceff91decd95f38f12
PtrHtn <ptrhtn@gmail.com>
2020-12-09 19:36:49 -0300

    Update landscape_layout of the result page

--- lib/pages/results/widgets/landscape_layout.dart
+++ lib/pages/results/widgets/landscape_layout.dart
@@ -18,19 +18,70 @@ class LandscapeLayoutBody extends GetView<ResultsController> {
   Widget build(BuildContext context) {
     return Container(
       color: Color(0xFF737373),
-      child: Container(
-        margin: EdgeInsets.all(40),
-        decoration: BoxDecoration(
-          color: Theme.of(context).cardColor,
-          borderRadius: BorderRadius.all(Radius.circular(20)),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            Padding(padding: const EdgeInsets.only(right: 25, left: 25)),
-          ],
-        ),
-      ),
+      child: Obx(() => Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          Expanded(
+            flex: 50,
+            child: Container(
+              margin: EdgeInsets.only(left: 20, top: 20, right: 20, bottom: 10),
+              decoration: BoxDecoration(
+                color: Theme.of(context).cardColor,
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+              child: Column(mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Dart\n"),
+                  controller.dartImageDecoded.value == false
+                      ? Text("Image decoding: ${controller.dartQueuingImageDecoding.value ? 'Queuing' : 'Processing'}\n")
+                      : Text(
+                      "Image decoding: ${controller.dartTimeOfImageDecoding.value} sec\n"),
+                  controller.dartJpgEncoded.value == false
+                      ? Text(
+                      "Jpg encoding: ${controller.dartQueuingJpgEncoding.value ? 'Queuing' : 'Processing'}\n")
+                      : Text(
+                      "Jpg encoding: ${controller.dartTimeOfJpgEncoding.value} sec\n"),
+                  controller.dartPngEncoded.value == false
+                      ? Text(
+                      "Png encoding: ${controller.dartQueuingPngEncoding.value ? 'Queuing' : 'Processing'}")
+                      : Text(
+                      "Png encoding: ${controller.dartTimeOfPngEncoding.value} sec")
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 50,
+            child: Container(
+              margin: EdgeInsets.only(left: 20, top: 10, right: 20, bottom: 20),
+              decoration: BoxDecoration(
+                color: Theme.of(context).cardColor,
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+              ),
+              child: Column(mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Java\n"),
+                  controller.javaImageDecoded.value == false
+                      ? Text("Image decoding: ${controller.javaQueuingImageDecoding.value ? 'Queuing' : 'Processing'}\n")
+                      : Text(
+                      "Image decoding: ${controller.javaTimeOfImageDecoding.value} sec\n"),
+                  controller.javaJpgEncoded.value == false
+                      ? Text(
+                      "Jpg encoding: ${controller.javaQueuingJpgEncoding.value ? 'Queuing' : 'Processing'}\n")
+                      : Text(
+                      "Jpg encoding: ${controller.javaTimeOfJpgEncoding.value} sec\n"),
+                  controller.javaPngEncoded.value == false
+                      ? Text(
+                      "Png encoding: ${controller.javaQueuingPngEncoding.value ? 'Queuing' : 'Processing'}")
+                      : Text(
+                      "Png encoding: ${controller.javaTimeOfPngEncoding.value} sec")
+                ],
+              ),
+            ),
+          )
+        ],
+      )),
     );
   }
 }
